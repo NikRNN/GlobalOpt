064b862b783884fe6148fd2e1641d5da222ad6a0
Nikita Romanov <romanovnnikita@gmail.com>
2023-12-24 00:51:15 +0300

    made btns and animate

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -3,7 +3,7 @@
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
-$message = $_POST['message']
+$message = $_POST['message'];
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -15,11 +15,11 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'muskatt07@mail.ru';                 // Наш логин
-$mail->Password = '8KZC87gpKFTwe435ZKMR';                           // Наш пароль от ящика
+$mail->Password = 'xJtYACzGpZun9nPK4iW7';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('muskatt07@mail.ru', 'Pulse');   // От кого письмо 
+$mail->setFrom('muskatt07@mail.ru', 'Global opt');   // От кого письмо 
 $mail->addAddress('romaanov-nikita@yandex.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
@@ -31,12 +31,28 @@ $mail->isHTML(true);                                  // Set email format to HTM
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
-		Пользователь оставил данные <br> 
+Пользователь оставил данные <br> 
+Имя: ' . $name . '                <br>
+Номер телефона: ' . $phone . '    <br>
+E-mail: ' . $email . '				<br>
+Сообщение: ' .$message .  '';         
+	
+	
+if(empty($message)) {
+	$mail->Body = '
+	Пользователь оставил данные <br> 
 	Имя: ' . $name . '                <br>
 	Номер телефона: ' . $phone . '    <br>
-	E-mail: ' . $email . '            <br>
-	Сообщение: '. $message .''
-	
+	E-mail: ' . $email . '' ;				
+	 
+} else {
+	$mail->Body    = '
+Пользователь оставил данные <br> 
+Имя: ' . $name . '                <br>
+Номер телефона: ' . $phone . '    <br>
+E-mail: ' . $email . '				<br>
+Сообщение: ' .$message .  '';  
+}
 
 if(!$mail->send()) {
     return false;
