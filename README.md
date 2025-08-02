b304645c854f3a41e49278b22af93d904508e588
Nikita R <romanovnnikita@gmail.com>
2025-08-02 17:00:55 +0300

    Сохранил локальные изменения

---
+++ README.md
@@ -0,0 +1,92 @@
+# Структура проекта
+
+**Основные каталоги:**
+
+```
+.
+├── dist/
+│   ├── css/
+│   ├── icons/
+│   │   ├── advantages/
+│   │   ├── carousel/
+│   │   ├── china/
+│   │   ├── contacts/
+│   │   ├── favicon/
+│   │   ├── job/
+│   │   ├── manager/
+│   │   ├── prices/
+│   │   ├── promo/
+│   │   └── scheme/
+│   ├── img/
+│   │   ├── china/
+│   │   ├── mmanager/
+│   │   ├── promo/
+│   │   └── questions/
+│   ├── js/
+│   ├── mailer/
+│   ├── index.html
+│   └── policy.html
+├── src/
+│   ├── icons/
+│   │   ├── advantages/
+│   │   ├── carousel/
+│   │   ├── china/
+│   │   ├── contacts/
+│   │   ├── favicon/
+│   │   ├── job/
+│   │   ├── manager/
+│   │   ├── prices/
+│   │   ├── promo/
+│   │   └── scheme/
+│   ├── img/
+│   │   ├── china/
+│   │   ├── manager/
+│   │   ├── promo/
+│   │   └── questions/
+│   ├── js/
+│   ├── mailer/
+│   ├── sass/
+│   │   ├── base/
+│   │   ├── blocks/
+│   │   └── libs/
+│   └── screenshots/
+├── gulpfile.json
+├── package.json
+├── package-lock.json
+└── README.md
+
+```
+
+=======
+
+# GlobalOpt (JavaScript + SCSS)
+
+![Интерфейс сайта](./src/screenshots/screenshot1.png)
+![Интерфейс сайта](./src/screenshots/screenshot2.png)
+![Интерфейс сайта](./src/screenshots/screenshot3.png)
+![Интерфейс сайта](./src/screenshots/screenshot4.png)
+![Интерфейс сайта](./src/screenshots/screenshot5.png)
+![Интерфейс сайта](./src/screenshots/screenshot6.png)
+![Интерфейс сайта](./src/screenshots/screenshot7.png)
+![Интерфейс сайта](./src/screenshots/screenshot8.png)
+
+## О проекте
+
+GlobalOpt - сайт компании международных грузоперевозок. На сайте реализован слайдер; данные, которые ввел пользователь в форме, отправятся на адрес электронной почты с помощью smart.php. С помощью media-запросов выполнена адаптация под средние и мобильные устройства. Использован таск-менеджер Gulp.
+
+## Технологии
+
+- JavaScript
+- gulp
+- SCSS
+- php отправка данных с формы на электронную почту
+- media-запросы
+
+## Установка и запуск
+
+```bash
+git clone https://github.com/NikRNN/GlobalOpt.git
+cd GlobalOpt
+npm install
+gulp
+```
---
+++ src/js/script.js
@@ -0,0 +1,159 @@
+//запуск jQuery (после того, как будет готов html-документ)
+$(document).ready(function () {
+  //настройка validate plugin
+
+  $(".prices__consultation-form").validate({
+    rules: {
+      name: "required",
+      tel: "required",
+      email: {
+        required: true,
+        email: true,
+      },
+      message: "required",
+    },
+    messages: {
+      name: "Пожалуйста, введите имя",
+      tel: "Пожалуйста, введите номер телефона",
+      email: {
+        required: "Пожалуйста, введите электронный адрес",
+        email: "Электронный адрес должен быть в формате example@domain.com",
+      },
+    },
+  }),
+    $(".questions-form").validate({
+      rules: {
+        name: "required",
+        tel: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+        message: "required",
+      },
+      messages: {
+        name: "Пожалуйста, введите имя",
+        tel: "Пожалуйста, введите номер телефона",
+        email: {
+          required: "Пожалуйста, введите электронный адрес",
+          email: "Электронный адрес должен быть в формате example@domain.com",
+        },
+        message: "Пожалуйста, укажите вопрос",
+      },
+    });
+
+  //настройка owlCarousel
+  $(".owl-carousel").owlCarousel({
+    items: 3,
+    loop: true,
+    autoplay: false,
+    nav: true,
+    dots: false,
+    center: true,
+    navText: [
+      "<img src='icons/carousel/btn_prev.png' />",
+      "<img src='icons/carousel/btn_next.png' />",
+    ],
+    responsive: {
+      991: {
+        items: 3,
+      },
+      769: {
+        items: 1,
+        center: true,
+        nav: true,
+        dots: false,
+      },
+      576: {
+        items: 1,
+        center: false,
+        nav: true,
+        dots: false,
+      },
+      320: {
+        items: 1,
+        center: false,
+        nav: false,
+        dots: true,
+        responsiveClass: true,
+        loop: false,
+      },
+    },
+  });
+
+  //настройка маски для ввода номера телефона - плагин maskedinput
+  $("input[name=tel]").mask("+7(999) 999-99-99");
+
+  //настройка отправки писем на сервер
+  $("form").submit(function (e) {
+    e.preventDefault(); //отменили стандартное поведение браузера при нажатии на кнопку (страница не перезагружается)
+
+    if (!$(this).valid()) {
+      return;
+    } //если поля в форме пустые, то этот код позволит прекратить функцию и не отправлять пустые данные
+
+    $.ajax({
+      type: "POST", //указываем, получаем мы данные с сервера или отправляем
+      url: "mailer/smart.php", //тут указали обработчик нашей отправки, это будет файл смарт php
+      data: $(this).serialize(), //указали, какие данные отправляем на сервер; для каждого e будут свои данные - this ссылается на каждый конкретный e; serialize - форматирует данные в понятный для сервера формат
+    }).done(function () {
+      $("#thanks").fadeIn("slow"); //после успешной отправки всплывает модальное окно с благодарностью
+      //тут указали, что делать дальше, когда запрос выполнен
+      $(this).find("input").val(""); //находим инпуты, их значения приводим к пустой строке
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  new WOW().init();
+
+  //закрываем модальное окно при нажатии на крестик
+  $(".modal__close").on("click", function () {
+    $("#thanks").fadeOut("slow");
+  });
+
+  //при нажатии на кнопку подробнее в ценах переворачивает слайд с информацией
+  $(".prices__btn").each(function (i) {
+    //тут мы выбираем все элементы с этим классом
+    $(this).on("click", function (e) {
+      //при клике на конкретный элемент отменяем поведение браузера
+      e.preventDefault();
+      $(".prices__item-content")
+        .eq(i) //ВЫБИРАЕМ КОНКРЕТНЫЙ ЭЛЕМЕНТ (А НЕ ВСЕ С УКАЗАННЫМ КЛАССОМ)
+        .toggleClass("prices__item-content_active"); //ДОБАВЛЯЕМ ИЛИ УБИРАЕМ КЛАСС
+      $(".prices__item-list").eq(i).toggleClass("prices__item-list_active");
+    });
+  });
+  //при нажатии на кнопку назад слайд возвращается
+  $(".prices__btn-back").each(function (i) {
+    $(this).on("click", function (e) {
+      e.preventDefault();
+      $(".prices__item-content")
+        .eq(i)
+        .toggleClass("prices__item-content_active");
+      $(".prices__item-list").eq(i).toggleClass("prices__item-list_active");
+    });
+  });
+});
+
+//настройка открытия меню при нажатии на гамбургер
+window.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".promo__menu"), //в переменную помещаем элемент с определенным классом
+    menuLink = document.querySelectorAll(".promo__menu-link"), // аналогично
+    hamburger = document.querySelector(".promo__hamburger"); //аналогично
+
+  hamburger.addEventListener("click", () => {
+    //добавили событие: при клике добавляется класс гамбургеру и меню
+    hamburger.classList.toggle("promo__hamburger-active");
+    menu.classList.toggle("promo__menu-active");
+  });
+
+  menuLink.forEach((item) => {
+    //для каждого элемента меню мы добавляем обработчик событий:при клике на элемент класс активности переключается, а меню закрывается
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("promo__hamburger-active");
+      menu.classList.toggle("promo__menu-active");
+    });
+  });
+});
